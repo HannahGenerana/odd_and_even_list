ca1adf2e5ee16adc9b6c0335d5c655befc7fae7f
HannahGenerana <hannahgenerana@gmail.com>
2024-01-24 21:18:41 +0800

    pseudocode

---
+++ odd_and_even_list.py
@@ -0,0 +1,17 @@
+# Exercise 10: Create a new list from two list using the following condition
+# Create a new list from two list using the following condition
+# Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+
+# pseudocode
+
+# create a function to merge both different list in the result
+
+# create an empty list to store the result
+
+# take all odd numbers on the first list
+
+# take all even numbers on the second list
+
+# return the result of both list
+
+# print the result
